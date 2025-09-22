2029960f2a3906857ed514b0ef40644afebb5d6a
Harjeet Singh Chahal <91168704+harjeet-chahal@users.noreply.github.com>
2025-09-22 13:11:11 -0400

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -3,25 +3,25 @@ from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import structural_similarity as ssim
 
 
-# Inputs expected in [-1,1], CxHxW torch tensors
-
-
 def to_img(x):
-# x: torch tensor [-1,1]
-x = x.detach().cpu().numpy()
-return np.transpose((x + 1.0) / 2.0, (1, 2, 0)).clip(0, 1)
-
-
+    """
+    Convert a torch tensor in [-1, 1] (CxHxW) to HxWxC numpy image in [0,1].
+    """
+    x = x.detach().cpu().numpy()
+    return np.transpose((x + 1.0) / 2.0, (1, 2, 0)).clip(0, 1)
 
 
 def compute_psnr(img1_t, img2_t):
-i1, i2 = to_img(img1_t), to_img(img2_t)
-return float(psnr(i1, i2, data_range=1.0))
-
-
+    """
+    Compute PSNR between two tensors.
+    """
+    i1, i2 = to_img(img1_t), to_img(img2_t)
+    return float(psnr(i1, i2, data_range=1.0))
 
 
 def compute_ssim(img1_t, img2_t):
-i1, i2 = to_img(img1_t), to_img(img2_t)
-# multichannel=True is deprecated; channel_axis=-1 instead
-return float(ssim(i1, i2, channel_axis=-1, data_range=1.0))
+    """
+    Compute SSIM between two tensors.
+    """
+    i1, i2 = to_img(img1_t), to_img(img2_t)
+    return float(ssim(i1, i2, channel_axis=-1, data_range=1.0))
